63ccbb4013d2619781092e981d99f0a54be9f0dd
Akshat Gupta <akshatgupta0043@gmail.com>
2024-09-01 20:58:08 +0530

    Update4

--- src/Pages/ArticlePage.js
+++ src/Pages/ArticlePage.js
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom"
 import articles from "./Article-Content"
+import NotFoundPage from "./NotFoundPage"
 
 
 const ArticlePage =()=>{
     const params=useParams()
     const {articleId}=params
     const article=articles.find(article=>article.name===articleId)
+
+
+    if(!article){
+        return <NotFoundPage/>
+    }
     return(
         <>
       <h1>{article.title}</h1>
